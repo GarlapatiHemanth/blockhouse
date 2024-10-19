5f9786a9a543ab84a9a4b83c74c628f4da4005c4
Hemanth Lakshmi Kanth Garlapati <hemanthlakshmikanthgarlapati@Hemanths-MacBook-Air.local>
2024-10-18 21:51:04 -0400

    updated allowed hosts

--- project_name/settings.py
+++ project_name/settings.py
@@ -29,7 +29,7 @@ class Base(Configuration):
 # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
 
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
