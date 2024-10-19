27ffee248692881abab0c61f31509bb8f5a79489
Hemanth Lakshmi Kanth Garlapati <hemanthlakshmikanthgarlapati@Hemanths-MacBook-Air.local>
2024-10-18 23:55:23 -0400

    updated readme and test cases, error handling

--- app/ml.py
+++ app/ml.py
@@ -4,21 +4,23 @@ import pandas as pd
 
 def trainmodel(data):
 
-    
-    df = list(data.values('date', 'close_price'))
-    df = pd.DataFrame(df)
-    
-    df['date_ordinal'] = pd.to_datetime(df['date']).map(pd.Timestamp.toordinal)
-    
-    
-    X = df['date_ordinal'].values.reshape(-1, 1)  
-    y = df['close_price'].values  
+    try:
+        df = list(data.values('date', 'close_price'))
+        df = pd.DataFrame(df)
+        
+        df['date_ordinal'] = pd.to_datetime(df['date']).map(pd.Timestamp.toordinal)
+        
+        
+        X = df['date_ordinal'].values.reshape(-1, 1)  
+        y = df['close_price'].values  
 
- 
-    model = LinearRegression()
-    model.fit(X, y)
     
-    with open('linear_regression_model.pkl', 'wb') as file:
-        pickle.dump(model, file)
-    
-    print("Model trained and saved as linear_regression_model.pkl")
+        model = LinearRegression()
+        model.fit(X, y)
+        
+        with open('linear_regression_model.pkl', 'wb') as file:
+            pickle.dump(model, file)
+        
+        print("Model trained and saved as linear_regression_model.pkl")
+    except Exception:
+        print(Exception)
